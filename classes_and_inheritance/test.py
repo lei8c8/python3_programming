0d956c9939bee98bbcab0383e23696c9db8da560
Lei Chen <lei8c8@gmail.com>
2020-07-02 18:53:36 -0400

    finish 4th course

---
+++ classes_and_inheritance/test.py
@@ -0,0 +1,14 @@
+def square(x):
+    return x * x
+
+import test 
+print('testing square function')
+test.testEqual(square(10), 100)
+
+class Student():
+    def __init__(s,a,b=1): s.name,s.years_UM,s.knowledge = ''*200+a+''*100,1,len(lr(0)) + len([])
+    def study(s):
+        for _ in lr(s.knowledge): s.knowledge = s.knowledge + 1
+    def getKnowledge(s):
+        for i in lr(s.knowledge): return s.knowledge
+    def year_at_umich(s): return s.years_UM
